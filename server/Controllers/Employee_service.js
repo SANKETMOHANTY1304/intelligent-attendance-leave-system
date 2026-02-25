d53677603f8a5923f6c23736b67a23473b8de834
Ashish <ashishthaku001@gmail.com>
2026-02-25 12:36:37 +0530

    Add employee module (models, Controllers, routes, app)

--- server/Controllers/Employee_service.js
+++ server/Controllers/Employee_service.js
@@ -1,1 +1,36 @@
 
+import Employee from "../models/Employee.js";
+import employeesData from "../employee.js";
+
+export  const employeelist =async(req,res)=>{
+    try {
+        await Employee.insertMany(employeesData, { ordered: false });
+        console.log("20 employee data inserted")
+        res.json({ message: "Employee data inserted successfully" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }   
+}
+
+
+ export const findEmployee = async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const employee = await Employee.findById(id);
+        if (!employee) {
+            return res.status(404).json({ error: "Employee not found" });
+        }
+        res.json(employee);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+ }
+
+export const findallEmployee= async(req,res)=>{
+    try {
+        const all=await Employee.find()
+        res.json(all);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
--- server/routes/Employees.js
+++ server/routes/Employees.js
@@ -1,1 +1,11 @@
 
+import express from 'express'
+import { employeelist, findallEmployee, findEmployee, } from '../services/Employee_service.js';
+
+const employeeRouter= express.Router();
+
+employeeRouter.post('/',employeelist)
+employeeRouter.get('/allemployee',findallEmployee)
+employeeRouter.get('/:id',findEmployee)
+
+export default employeeRouter
