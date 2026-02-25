7ac4c6ebbd5fe1672cbcea77478862b52a7be619
Ashish <ashishthaku001@gmail.com>
2026-02-25 22:32:04 +0530

    Add employee module (models, Controllers, routes, app)

--- server/routes/Adminroutes.js
+++ server/routes/Adminroutes.js
@@ -1,1 +1,9 @@
+import express from "express";
+import { adminLogin } from "../services/adminController.js";
 
+const adminRouter = express.Router();
+
+// POST /api/admin/login → Admin login with token generation
+adminRouter.post("/login", adminLogin);
+
+export default adminRouter;
